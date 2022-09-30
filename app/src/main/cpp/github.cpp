770d6bb0c2b0aa7922d8d771cd8de3015b2e4b46
herdianurdin <herdianurdin@gmail.com>
2022-09-30 20:46:09 +0700

    fix Bearer token

--- app/src/main/cpp/github.cpp
+++ app/src/main/cpp/github.cpp
@@ -3,14 +3,12 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_saeware_github_config_Config_apiKey(JNIEnv *env, jobject object) {
-   std::string api_key = "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";
-
+   std::string api_key = "Bearer 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";
    return env->NewStringUTF(api_key.c_str());
 }
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_saeware_github_config_Config_baseUrl(JNIEnv *env, jobject object) {
    std::string base_url = "https://api.github.com/";
-
    return env->NewStringUTF(base_url.c_str());
 }
